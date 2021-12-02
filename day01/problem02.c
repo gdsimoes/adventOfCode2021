54dca580fd5e1e73027c30cd95bc4ce6ac812174
Guilherme Dias Simoes <gds.simoes@gmail.com>
2021-12-02 14:44:24 +0000

    Add problem 2

--- day01/problem02.c
+++ day01/problem02.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #define LINE_SIZE 6
+#define WINDOW_SIZE 3
 
 int main(void)
 {
@@ -14,22 +15,28 @@ int main(void)
     }
 
     char line[LINE_SIZE];
-    int prev, curr;
+    int window[WINDOW_SIZE];
     int count = 0;
 
-    // Read the first line
-    fgets(line, sizeof(line), input);
-    prev = atoi(line);
+    // Read the first few lines. I'm assuming the file is big enough.
+    for (int i = 0; i < WINDOW_SIZE; i++)
+    {
+        fgets(line, sizeof(line), input);
+        window[i] = atoi(line);
+    }
 
-    // Read the rest of the file and count how many measurements are larger than the previous one
+    // Read the rest of the file and count how many measurement windows are larger than the previous one
+    int i = 0;
     while (fgets(line, sizeof(line), input))
     {
-        curr = atoi(line);
-        if (curr > prev)
+        int curr = atoi(line);
+        if (curr > window[i])
         {
             count++;
         }
-        prev = curr;
+        // Here we wrap around the array to avoid any swapping of elements
+        window[i] = curr;
+        i = (i + 1) % WINDOW_SIZE;
     }
 
     // Print the result
